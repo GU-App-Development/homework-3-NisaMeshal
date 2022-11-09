6cc5db45a21bcf96f97129e6a3d42abfa60ea612
Nisa Meshal <nisameshal@gmail.com>
2022-11-09 14:37:28 -0800

    other activities added and button listeners implemented

--- app/src/main/java/com/hw3/comboapp/MainActivity.kt
+++ app/src/main/java/com/hw3/comboapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.hw3.comboapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    fun onPizzaClick(view: View) {
+        val intent = Intent(this, PizzaParty::class.java)
+        startActivity(intent)
+    }
 
+    fun onLightsoutClick(view: View) {
+        val intent = Intent(this, LightsOut::class.java)
+        startActivity(intent)
+    }
 }
